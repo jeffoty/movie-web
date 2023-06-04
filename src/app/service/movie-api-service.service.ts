f591c65ee69a90f18d83bb9e860ccb249fb03dd4
jeffoty <odhiambo47@gmail.com>
2023-06-04 18:54:04 +0300

    ✨feat:movie api service for fetching data from API

---
+++ src/app/service/movie-api-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieApiServiceService {
+
+  constructor(private http:HttpClient) { }
+
+  baseurl = 'http://api.themoviedb.org/3';
+  apiKey = '439e8a1e870ca883607120eee4d94815'
+
+  // bannerapi data
+  
+  bannerApiData():Observable<any> {
+    return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apiKey}`)
+  }
+
+  //trending movie api data
+
+  trendingMovieApiData():Observable<any> {
+    return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apiKey}`)
+  }
+}
