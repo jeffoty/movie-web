31e4ff1c2d49d83ba545d650b42650fc555d1e59
jeffoty <odhiambo47@gmail.com>
2023-06-04 18:44:26 +0300

    🚚 app routing for application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+
+const routes: Routes = [
+  { path: '', component:HomeComponent},
+  { path: 'search', component:SearchComponent},
+  { path: 'movie/:id', component:MovieDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
