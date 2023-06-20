cd4ab1ed9e99644ec043b63bc40fac44e4ec911d
jeffoty <odhiambo47@gmail.com>
2023-06-20 22:28:03 +0300

    ♻️refactored code in the movie details components.

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,5 +9,47 @@ import { Component } from '@angular/core';
 })
 export class MovieDetailsComponent {
 
+  constructor(private service:MovieApiServiceService, private router:ActivatedRoute){}
 
+  movieDetailsResults:any;
+  movieVideoResult:any;
+  movieCastResult:any;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    let getParamId = this.router.snapshot.paramMap.get('id')
+    console.log(getParamId,'param id');
+
+    this.getMovie(getParamId);
+    this.getMovieVideo(getParamId);
+    this.getMovieCast(getParamId)
+
+  }
+
+  getMovie(id:any){
+    this.service.movieDetailsApiData(id).subscribe(data =>{
+      console.log(data,'movie details');
+      this.movieDetailsResults = data;
+    })
+  }
+
+  getMovieVideo(id:any){
+    this.service.movieVideoApiData(id).subscribe(data =>{
+      console.log(data, '')
+      data.results.forEach((element:any) => {
+        if (element.type = 'Tr') {
+          this.movieVideoResult = element.key;
+        }
+      });
+     
+    })
+  }
+
+  getMovieCast(id:any){
+    this.service.movieCastApiData(id).subscribe(data =>{
+      console.log(data, '')
+      this.movieCastResult = data.cast;
+    })
+  }
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -13,18 +13,32 @@ export class MovieApiServiceService {
   apiKey = '439e8a1e870ca883607120eee4d94815'
 
   // bannerapi data
-  
   bannerApiData():Observable<any> {
     return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apiKey}`)
   }
 
   //trending movie api data
-
   trendingMovieApiData():Observable<any> {
     return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apiKey}`)
   }
 
+  //movie search
   getSearchMovie(data:any):Observable <any> {
     return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apiKey}&query=${data.movieName}`)
+  }
+
+  //movie details api data
+  movieDetailsApiData(data:any):Observable<any> {
+    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apiKey}`)
+  }
+
+  //movie video api data
+  movieVideoApiData(data:any):Observable<any> {
+    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apiKey}`)
+  }
+
+  //movie cast
+  movieCastApiData(data:any):Observable<any> {
+    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apiKey}`)
   }
 }
