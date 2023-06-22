99b3e59e8181367c78570c0ca293f06a443e3978
jeffoty <odhiambo47@gmail.com>
2023-06-22 11:10:47 +0300

    ♻️ refactored code.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,13 @@ export class HomeComponent {
 
   bannerResult: any=[];
   trendingMovieResult: any=[];
+  actionMovieResult: any=[];
+  adventureMovieResult: any=[];
+  animationMovieResult: any=[];
+  comedyMovieResult: any=[];
+  documentaryMovieResult: any=[];
+  fictionMovieResult: any=[];
+  thrillerMovieResult: any=[];
 
   constructor(private service:MovieApiServiceService) {}
 
@@ -18,12 +25,19 @@ export class HomeComponent {
     //Add 'implements OnInit' to the class.
     this.bannerData();
     this.trendingMovieData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.animationMovie();
+    this.comedyMovie();
+    this.documentaryMovie();
+    this.fictionMovie();
+    this.thrillerMovie();
   }
 
   //banner data
   bannerData(){
     this.service.bannerApiData().subscribe((data) =>{
-      console.log(data, 'bannerdata#');
+      // console.log(data, 'bannerdata#');
       this.bannerResult=data.results;
     })
   }
@@ -31,8 +45,64 @@ export class HomeComponent {
   // trending movie data
   trendingMovieData(){
     this.service.trendingMovieApiData().subscribe((data) =>{
-      console.log(data, "trendingMovies#");
+      // console.log(data, "trendingMovies#");
       this.trendingMovieResult=data.results;
     });
   } 
+
+  //action
+  actionMovie(){
+    this.service.getActionMovies().subscribe((data) =>{
+      // console.log(data, "action#");
+      this.actionMovieResult = data.results;
+    })
+  }
+
+  //adventure
+  adventureMovie(){
+    this.service.getAdventureMovies().subscribe((data) =>{
+      // console.log(data, "adventure#");
+      this.adventureMovieResult = data.results;
+    })
+  }
+
+  //animtion
+  animationMovie(){
+    this.service.getAnimationMovies().subscribe((data) =>{
+      // console.log(data, "animation#");
+      this.animationMovieResult = data.results;
+    })
+  }
+
+  //comedy
+  comedyMovie(){
+    this.service.getComedyMovies().subscribe((data) =>{
+      // console.log(data, "comedy#");
+      this.comedyMovieResult = data.results;
+    })
+  }
+
+  //documentary
+  documentaryMovie(){
+    this.service.getDocumentaryMovies().subscribe((data) =>{
+      // console.log(data, "documentary#");
+      this.documentaryMovieResult = data.results;
+    })
+  }
+
+  //fiction
+  fictionMovie(){
+    this.service.getFictionMovies().subscribe((data) =>{
+      // console.log(data, "fiction#");
+      this.fictionMovieResult = data.results;
+    })
+  }
+
+  //thriller
+  thrillerMovie(){
+    this.service.getThrillerMovies().subscribe((data) =>{
+      // console.log(data, "thriller#");
+      this.thrillerMovieResult = data.results;
+    })
+  }
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -10,35 +10,70 @@ export class MovieApiServiceService {
   constructor(private http:HttpClient) { }
 
   baseurl = 'http://api.themoviedb.org/3';
-  apiKey = '439e8a1e870ca883607120eee4d94815'
+  apiKey = '439e8a1e870ca883607120eee4d94815';
 
   // bannerapi data
   bannerApiData():Observable<any> {
-    return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apiKey}`)
+    return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apiKey}`);
   }
 
   //trending movie api data
   trendingMovieApiData():Observable<any> {
-    return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apiKey}`)
+    return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apiKey}`);
   }
 
   //movie search
   getSearchMovie(data:any):Observable <any> {
-    return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apiKey}&query=${data.movieName}`)
+    return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apiKey}&query=${data.movieName}`);
   }
 
   //movie details api data
   movieDetailsApiData(data:any):Observable<any> {
-    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apiKey}`)
+    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apiKey}`);
   }
 
   //movie video api data
   movieVideoApiData(data:any):Observable<any> {
-    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apiKey}`)
+    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apiKey}`);
   }
 
   //movie cast
   movieCastApiData(data:any):Observable<any> {
-    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apiKey}`)
+    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apiKey}`);
+  }
+
+  //action movies
+  getActionMovies():Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apiKey}&with_genres=28`);
+  }
+
+  //advennture movies
+  getAdventureMovies():Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apiKey}&with_genres=12`);
+  }
+
+  //animation movies
+  getAnimationMovies():Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apiKey}&with_genres=16`);
+  }
+
+  //comedy movies
+  getComedyMovies():Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apiKey}&with_genres=35`);
+  }
+
+  //documentary movies
+  getDocumentaryMovies():Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apiKey}&with_genres=99`);
+  }
+
+  //fiction movies
+  getFictionMovies():Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apiKey}&with_genres=878`);
+  }
+
+  //thriller movies
+  getThrillerMovies():Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apiKey}&with_genres=53`);
   }
 }
